9205c5afa7694bbd6f2afbc173e1a818ddc6d0af
lololoco159159159 <109400548+lololoco159159159@users.noreply.github.com>
2023-01-22 03:17:24 -0300

    Add files via upload

--- lib/uteis.h
+++ lib/uteis.h
@@ -4,8 +4,7 @@
 typedef int (*Vetor_Get_or_Set_fn)(char acao[], int valor);
 
 void ExibeMenu();
-void ExecutaOpcaoUsuario(tDocumento **pp_Docs, tPalavra **pp_Palavras, int opcao);
-
+void ExecutaOpcaoUsuario(tDocumento **pp_Docs, tPalavra **pp_Palavras, int opcao, int k);
 int LeQuantidadeDeArquivos(FILE* fArquivo_caminho_noticias);
 void ConfereEntradaValida(char entrada[], char modo[]);
 void ResetaString(char str[]);
@@ -26,10 +25,32 @@ int Armazena_Genero_Array(char **pp_TodosGeneros, char **pp_UnicoGeneros, char *
 void ConfereProg1FoiRodado(char entrada[], char modo[]);
 void Buscador(tDocumento** pp_Docs, tPalavra** pp_Palavras);
 int Cmp_TF_IDF(const void* f1, const void* f2);
-
 int Calcula_Frequencia_Palavra_no_TipoNoticia(tDocumento** pp_Docs, tPalavra* p_Palavra, char genero[], int qtd_docs);
 int Cmp_Freq(const void* f1, const void* f2);
-
+void LiberacaoAuxiliaresRelatorioPalavra(char **pp_TodosGeneros, char **pp_UnicoGeneros, int *freq, int *freq_aux, int *acessado);
 double calculaSomatorio_TF_IDF(tDocumento* p_Doc, tPalavra** pp_Palavras , int *idx_palavra, int qtd_palavras_buscador, int idx_documento);
+void LiberaAuxiliaresClassificador(tDocumento *pDoc_Digitadas, int *idx_palavra, double *p_frequencia, double *pResultadosCos, double *p_aux_ResultadosCos, int *pAcessados);
+
+void Classificador(tDocumento** pp_Docs, tPalavra** pp_Palavras, int k);
+double CalculaDistanciaPorCos(tDocumento* pDoc_Digitadas, tDocumento* p_Doc, int idx_doc, tPalavra** pp_Palavras, double* p_frequencia, int qtd_palavras_classificador);
+int VerificaPalavraJaDigitada(int* idx_palavra, int idx_aux, int qtd_palavras_classificador);
+double CalculaSomatorioNumerador(tDocumento *pDoc_Digitadas, tDocumento *p_Doc,  int qtd_palavras_classificador, double *a, double *b);
+double CalculaSomatorioDenominador(tDocumento *pDoc_Digitadas, tDocumento *p_Doc, double* p_TFIDF_palavra_em_doc, int qtd_palavras_classificador);
+int Cmp_Distancia_Docs(const void *d1, const void *d2);
+int GaranteOpcaoValida();
+int OpcaoValida(int opcao);
+
+
+/*
+                            X
+             ---------------^---------------
+             E(tf-idf(w,dx) * tfidf-(w, di))
+        ________________________________________
+
+    sqrt[E(tf-idf(w,d1))] * sqrt[E(tf-idf(w, d2))]
+    ----------v------------   -----------v------------
+              y1                        y2
+
+*/
 
 #endif
